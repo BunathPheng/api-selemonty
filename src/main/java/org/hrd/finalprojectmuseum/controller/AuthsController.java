170acad781a6e4431136365bf2aa6c82806abd09
pichet <good100vichet@gmail.com>
2025-06-06 00:31:31 +0700

    update sending email template

--- src/main/java/org/hrd/finalprojectmuseum/controller/AuthsController.java
+++ src/main/java/org/hrd/finalprojectmuseum/controller/AuthsController.java
@@ -166,7 +166,7 @@ public class AuthsController {
 
     @Operation(summary = "For send re-send otp to verify account", description = "This endpoint use for send otp to verify account if user request to resend again")
     @PostMapping("/resend-otp")
-    public ResponseEntity<ApiResponse<Otps>> sendOtp(@RequestParam @Email(message = "Email form is incorrect") @NotBlank(message = "Email is required") String email) {
+    public ResponseEntity<ApiResponse<Otps>> sendOtp(@RequestParam @Email(message = "Email form is incorrect") @NotBlank(message = "Email is required") String email) throws IOException {
         String otp = sendEmailService.generateOtp();
         appUserService.checkEmailBeforeOpt(email);
 //        try {
@@ -214,7 +214,7 @@ public class AuthsController {
 
     @Operation(summary = "For forgot password feature", description = "After input email, OTP will send to email. Then use OTP to verify in verify-otp/forgot-password endpoint. NOTE: if you dont see OTP email send in inbox please kinda check in spam. ")
     @PostMapping("/forgot-password")
-    public ResponseEntity<ApiResponse<Otps>> forgotPassword(@RequestBody @Valid ForgotPasswordRequest forgotPasswordRequest) {
+    public ResponseEntity<ApiResponse<Otps>> forgotPassword(@RequestBody @Valid ForgotPasswordRequest forgotPasswordRequest) throws IOException {
         String otp = sendEmailService.generateOtp();
         appUserService.checkEmail(forgotPasswordRequest.getEmail());
 //        try {
