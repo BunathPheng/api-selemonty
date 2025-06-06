0e58e1a5b047454882477c84d7c8cf802d354657
REACH-DEL <tirachlo34@gmail.com>
2025-06-06 08:40:51 +0700

    update server

--- src/main/java/org/hrd/finalprojectmuseum/config/SwaggerConfig.java
+++ src/main/java/org/hrd/finalprojectmuseum/config/SwaggerConfig.java
@@ -18,7 +18,7 @@ import org.springframework.context.annotation.Configuration;
                 )
         ),
         servers = {
-                @Server(url = "http://34.143.146.124:8085", description = "Local Server"),
+                @Server(url = "http://34.143.146.124:9125", description = "Test Server"),
         }
 )
 public class SwaggerConfig {
