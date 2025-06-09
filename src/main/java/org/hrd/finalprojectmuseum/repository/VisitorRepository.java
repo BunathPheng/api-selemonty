4fa7ad05f6a0995f546e7db5ba34d17ce8b89bad
REACH-DEL <tirachlo34@gmail.com>
2025-06-09 08:55:28 +0700

    update

--- src/main/java/org/hrd/finalprojectmuseum/repository/VisitorRepository.java
+++ src/main/java/org/hrd/finalprojectmuseum/repository/VisitorRepository.java
@@ -26,7 +26,7 @@ public interface VisitorRepository {
         SELECT * FROM visitors v INNER JOIN bookings b
         ON v.visitor_id = b.visitor_id
         WHERE b.museum_id = #{museumId}::UUID
-        AND full_name ILIKE CONCAT('%', #{seach}, '%')
+        AND full_name ILIKE CONCAT('%', #{search}, '%')
         OFFSET (#{page}-1)* #{size} LIMIT #{size};
     """)
     List<Visitor> findVisitorByMuseumId(UUID museumId, String search, Integer page, Integer size);
@@ -35,14 +35,14 @@ public interface VisitorRepository {
         SELECT COUNT(*) FROM visitors v INNER JOIN bookings b
         ON v.visitor_id = b.visitor_id
         WHERE b.museum_id = #{museumId}::UUID
-        AND full_name ILIKE CONCAT('%', #{seach}, '%')
+        AND full_name ILIKE CONCAT('%', #{search}, '%')
     """)
     Integer countAllVisitor(UUID museumId, String search);
 
     @ResultMap("visitorMapper")
     @Select("""
         SELECT * FROM visitors
-        WHERE full_name ILIKE CONCAT('%', #{seach}, '%')
+        WHERE full_name ILIKE CONCAT('%', #{search}, '%')
         OFFSET (#{page}-1)* #{size} LIMIT #{size};
     """)
     ListResponse<Visitor> findAllVisitor(String search, Integer page, Integer size);
